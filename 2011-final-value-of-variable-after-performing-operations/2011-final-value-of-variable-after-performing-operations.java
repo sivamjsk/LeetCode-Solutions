cc80844b398f393563184c67c4e28f128db3acbf
Sivakumaran M J <30899915+sivamjsk@users.noreply.github.com>
2022-02-20 11:08:59 -0500

    Time: 1 ms (90.10%), Space: 43.7 MB (7.70%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int count = 0;
+        for(String s : operations){
+            if(s.equals("++X") || s.equals("X++"))
+                count++;
+            else if(s.equals("--X") || s.equals("X--"))
+                count--;    
+        }
+        return count;
+        
+    }
+}
