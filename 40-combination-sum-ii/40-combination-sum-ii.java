f57912022c736ae8e7bf085cf96bb2db5476b727
Sivakumaran M J <30899915+sivamjsk@users.noreply.github.com>
2022-03-30 12:48:02 -0400

    Time: 8 ms (39.47%), Space: 43.7 MB (65.78%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<>();
+        generateSubsets(0,candidates,new ArrayList(),res,target);
+        return res;
+        
+    }
+    void generateSubsets(int index,int[] candidates,List<Integer> curr,List<List<Integer>>  res,int target){
+        
+        if(target == 0){
+            res.add(new ArrayList(curr));
+            return;
+        }
+        if(target<0) return;
+        for(int i=index;i<candidates.length;i++){
+            if(i>index && candidates[i-1]==candidates[i]) continue;
+            curr.add(candidates[i]);
+            generateSubsets(i+1,candidates,curr,res,target-candidates[i]);
+            curr.remove(curr.size()-1);
+        }
+    }
+}
